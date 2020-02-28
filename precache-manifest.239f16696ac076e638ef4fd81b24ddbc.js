aa778377a6a9f1069764eafc543315496cb9605e
Carlos <carlos.roldan@ludiumlab.com>
2020-02-28 20:05:17 +0100

    Updates

---
+++ precache-manifest.239f16696ac076e638ef4fd81b24ddbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e77f3c5daf825defe8a1162a11134c55",
+    "url": "/MRB_Pokedex/index.html"
+  },
+  {
+    "revision": "4005b7318df7b510de38",
+    "url": "/MRB_Pokedex/static/css/main.0d651481.chunk.css"
+  },
+  {
+    "revision": "0cce22b6e11f932794e8",
+    "url": "/MRB_Pokedex/static/js/2.551815f8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/MRB_Pokedex/static/js/2.551815f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4005b7318df7b510de38",
+    "url": "/MRB_Pokedex/static/js/main.9b87a494.chunk.js"
+  },
+  {
+    "revision": "ae5f07262ac11f9679a5",
+    "url": "/MRB_Pokedex/static/js/runtime-main.b14fa32f.js"
+  }
+]);
